092f6fb589341f7c34add0b213266b0504093740
Shunsuke Toba <boushi_sukaimu@yahoo.co.jp>
2016-09-24 13:39:07 +0900

    機能追加

--- peinto.js
+++ peinto.js
@@ -3,6 +3,7 @@ $(function() {
   var canvas = document.getElementById('mycanvas');
   if(!canvas || !canvas.getContext) return false;
   var ctx = canvas.getContext('2d');
+  ctx.lineCap = "round";
 
   var startX,
       startY,
@@ -10,15 +11,16 @@ $(function() {
       y,
       borderWidth = 10,
       isDrawing = false,
-      cStep = -1;
+      step = -1; //redo undo用
+      thse = 0; //サムネイル用
 
-  var cPushArray = new Array();
+  var car = new Array();
+  var thcar = new Array();
 
   $('#mycanvas').mousedown(function(e) {
     isDrawing = true;
     startX = e.pageX - $(this).offset().left - borderWidth;
     startY = e.pageY - $(this).offset().top - borderWidth;
-    undoImage = ctx.getImageData(0, 0,canvas.width,canvas.height);
   })
   .mousemove(function(e) {
     if(!isDrawing) return;
@@ -33,61 +35,105 @@ $(function() {
   })
   .mouseup(function() {
     isDrawing = false;
+    cPush();
   })
   .mouseleave(function() {
     isDrawing = false;
+    cPush();
   });
 
+//ペンの色
   $('li').click(function() {
     ctx.strokeStyle = $(this).css('background-color');
   });
 
+//ペンの太さ
   $('#penWidth').change(function() {
     ctx.lineWidth = $(this).val();
   });
 
+//透明度
   $('#pentoumei').change(function() {
     ctx.globalAlpha = $(this).val();
   });
 
+//画面消去　起動中
   $('#erase').click(function() {
     ctx.clearRect( 0, 0,canvas.width, canvas.height);
   });
 
+//画面消去　停止中
   $('#clear').click(function(e) {
     if(!confirm('本当に消去しますか？')) return;
       e.preventDefault();
       ctx.clearRect(0, 0, canvas.width, canvas.height);
   });
 
-  $('#undo').click(function(e) {
-    ctx.putImageData(undoImage,0,0);
+//redo undo
+  function cPush() {
+      step++;
+      if (step < car.length) { car.length = step; }
+      car.push(document.getElementById('mycanvas').toDataURL());
+  }
+
+//やり直し
+  $('#undo').click(function() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    if (step > 0) {
+        step--;
+        console.log(step);
+        var canvasPic = new Image();
+        canvasPic.src = car[step];
+        canvasPic.onload = function () { ctx.drawImage(canvasPic, 0, 0); }
+    }
   });
 
+//元に戻す
+  $('#redo').click(function() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    if (step < car.length-1) {
+        step++;
+        console.log(step);
+        var canvasPic = new Image();
+        canvasPic.src = car[step];
+        canvasPic.onload = function () { ctx.drawImage(canvasPic, 0, 0); }
+    }
+  });
 
+//画面保存
   $('#save').click(function() {
+    thse++;
     var img = $('<img>').attr({
       width: 250,
       height: 150,
       src: canvas.toDataURL()
     });
     var link = $('<a>').attr({
-      href: canvas.toDataURL().replace('image/jpg', 'application/octet-stream'),
-      download: new Date().getTime() + '.jpg'
+      href: canvas.toDataURL().replace('image/png', 'application/octet-stream'),
+      download: new Date().getTime() + '.png'
     });
     $('#gallery').append(link.append(img.addClass('thumbnail')));
     ctx.clearRect( 0, 0,canvas.width, canvas.height);
-
   });
 
+//サムネイルからcanvasへ
+  $('.thumbnail').dblclick(function() {
+    var thumcan = new Image();
+    canvasPic.src = car[step];
+    thumcan.onload = function () { ctx.drawImage(thumcan, 0, 0); }
+  });
+
+//未実装　画面サイズの変更
   $('#size').change(function() {
     ctx.width = '100px';
   });
 
+//ペンの形
   $('#linekata').change(function() {
     ctx.lineCap = $(this).val();
   });
 
+//ペンの角の形
   $('#lineori').change(function() {
     ctx.lineJoin = $(this).val();
   });
